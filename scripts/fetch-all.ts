8be2bedbc0ea66a766b8c35706c55517b64c93ce
Madoc <165958580+MadocVZ@users.noreply.github.com>
2025-08-28 13:41:05 +0200

    data(life_expectancy): add World Bank pipeline (SP.DYN.LE00.IN); update METHODS & CHANGELOG

--- scripts/fetch-all.ts
+++ scripts/fetch-all.ts
@@ -1,7 +1,11 @@
 import { run as co2 } from './pipelines/co2.ts';
+import { run as life_expectancy } from './pipelines/life_expectancy.ts';
 
 export async function runAll() {
-  const pipelines = [{ name: 'co2_ppm', run: co2 }];
+  const pipelines = [
+    { name: 'co2_ppm', run: co2 },
+    { name: 'life_expectancy', run: life_expectancy },
+  ];
   for (const p of pipelines) {
     console.log(`start ${p.name}`);
     const data = await p.run();
